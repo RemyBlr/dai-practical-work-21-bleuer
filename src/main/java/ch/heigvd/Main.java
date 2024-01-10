13405c1600702d2aada8a785808dedc6d0b616f7
Remy <remy.bleuer@hotmail.com>
2024-01-10 19:43:07 +0100

    End practical work

--- src/main/java/ch/heigvd/Main.java
+++ src/main/java/ch/heigvd/Main.java
@@ -1,7 +1,38 @@
 package ch.heigvd;
 
+import ch.heigvd.auth.AuthController;
+import ch.heigvd.users.User;
+import ch.heigvd.users.UsersController;
+import io.javalin.Javalin;
+
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Main {
+
+    public final static int PORT = 8081;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Javalin app = Javalin.create();
+
+        // This will serve as our database
+        ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+
+        // Controllers
+        AuthController authController = new AuthController(users);
+        UsersController usersController = new UsersController(users);
+
+        // Auth routes
+        app.post("/login", authController::login);
+        app.post("/logout", authController::logout);
+        app.get("/profile", authController::profile);
+
+        // Users routes
+        app.post("/users", usersController::create);
+        app.get("/users", usersController::getMany);
+        app.get("/users/{id}", usersController::getOne);
+        app.put("/users/{id}", usersController::update);
+        app.delete("/users/{id}", usersController::delete);
+
+        app.start(PORT);
     }
 }
